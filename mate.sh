0a45f5519e0cf39f6a9119f7a3d2de6df9003b30
sapasaja <94762995+sapasaja@users.noreply.github.com>
2022-01-02 17:59:46 +0700

    Update mate.sh

--- mate.sh
+++ mate.sh
@@ -1,11 +1,9 @@
 rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Ctrl + V Authtoken: " CRP 
+read -p ">>bismillah>>: " CRP 
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 3389 &>/dev/null &
-echo Downloading File From akuh.net
 sudo apt update && apt upgrade -y
 sudo apt install firefox -y
 sudo apt-get install -y xrdp
@@ -13,8 +11,5 @@ sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon
 sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
 sudo service xrdp start
 clear
-echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "More  Free VPS akuh.net"
-echo "===================================="
+sleep 9876543210
