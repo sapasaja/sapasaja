eb1e9edfd827861e234079a572fec989d0e566a1
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-05 03:29:46 +0700

    Create stb.sh

---
+++ stb.sh
@@ -0,0 +1,24 @@
+apt-get update
+wget -O atv+Xperience-sky.img https://bit.ly/3pFFVSq
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/3zvqiRj > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3388 &>/dev/null &
+clear
+apt-get install qemu-system-x86 -y
+qemu-system-x86_64 -hda atv+Xperience-sky.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+clear
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+sleep 4320000
