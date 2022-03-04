f396081b7b28a31b7faa80404e3d7bdf0bae712e
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-05 03:49:29 +0700

    Update stb.sh

--- stb.sh
+++ stb.sh
@@ -1,5 +1,5 @@
 apt-get update
-wget -O atv+Xperience-sky.img https://bit.ly/3pFFVSq
+wget -O 3MqXcIM.img https://bit.ly/3MqXcIM
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/3zvqiRj > /dev/null 2>&1
 chmod +x ng.sh
@@ -16,7 +16,7 @@ read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 3388 &>/dev/null &
 clear
 apt-get install qemu-system-x86 -y
-qemu-system-x86_64 -hda atv+Xperience-sky.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda 3MqXcIM.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
