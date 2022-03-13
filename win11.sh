697c80ee66204517ebd281ff2e2b46bdaf6ce3d4
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-13 20:05:32 +0700

    Update win11.sh

--- win11.sh
+++ win11.sh
@@ -1,6 +1,6 @@
 echo "####################################❤❖﷽❖❤#####################################"
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
-curl -L -o win11.zip https://bit.ly/3i4fsJS
+curl -L -o win11.zip https://public.ch.files.1drv.com/y4mZOhs57US01gbY4reSxpkOitywJLK52VTzTzzEuEPP3UcEsfkVHFEOQQZo2UItYnx02G77nZJItRMZ3TL8wIvLhLx4OV8o0BX6GwS2KCWxLQVpqtOfRGHgGqZKsRwfmkZ7IjrgVXH9H3_3ny5rah2XJNk87beX_6zjlFoWHAekhyQ2Z1Fmc8-KDzTxMIH0MbdkX5J8jn6s5BxfLU5p1GHeZNU2UtA0oSOurf-3iyFJ40
 unzip win11.zip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip &>/dev/null &
