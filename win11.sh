8c6fa9afa8bd926c7de37080e0b0d343e1518721
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-14 02:38:17 +0700

    Update win11.sh

--- win11.sh
+++ win11.sh
@@ -1,6 +1,6 @@
 echo "####################################❤❖﷽❖❤#####################################"
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
-wget -O win.img https://bit.ly/3t5PMTn
+wget -O win.img https://vagrantcloud-files-production.s3-accelerate.amazonaws.com/archivist/boxes/d8363da1-6f74-49a6-8a1b-378d9bd877de?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIA6NDPRW4BTF6WXXXK%2F20220313%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220313T193448Z&X-Amz-Expires=900&X-Amz-Security-Token=FwoGZXIvYXdzEOX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDDb%2BorXB5sGhrjmL0CK3Af5vcfZXScCZsNKNCUBlGl9huBrEKqgRsGzTz3jjUo%2B6XSAtdkFc%2FArh24KJQ%2BEWNfYgnM8s4Jx55BF2KWryxmqFzxN1iqJwYpHRgg7FBEmJz9CDWIaZdExzt14F0n0AUU%2F7kOm1gOJEuPP9%2Bzanj3pwD104ETbjV3TzGral7aTf6vYMYt9Ah1eLR%2B5LQW4OuINOmf0Unk4sIXEe6oy7yhmzM7jQagjD5GF1RU3GdYuTho2W30qgfSiRiLmRBjItnGG5X0fDoqsb7TeJ7UtxiSw84upZxie%2FCTlVXeVs79fbwhYwm5tHsOeTiA8C&X-Amz-SignedHeaders=host&X-Amz-Signature=a727b0ea7503ed64a5fe9d0215913e8bafa628a157c21bcb3b0520a7a50a4c52
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip &>/dev/null &
 unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
