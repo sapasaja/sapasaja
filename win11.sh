b1c2f56143babbe462f4f1284da6a815326a844c
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-14 03:00:52 +0700

    Update win11.sh

--- win11.sh
+++ win11.sh
@@ -1,6 +1,6 @@
 echo "####################################❤❖﷽❖❤#####################################"
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
-wget -O win.img https://bit.ly/34H4AyG
+wget -O win11.img https://bit.ly/34H4AyG
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip &>/dev/null &
 unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
