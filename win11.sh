3049bb3b6c7dfe5f270f6d9145df25929ed510fe
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-13 10:48:02 +0700

    Create win11.sh

---
+++ win11.sh
@@ -0,0 +1,16 @@
+echo "####################################❤❖﷽❖❤#####################################"
+echo "********************************(khasbunalloh wanikmal wakil)*******************************"
+curl -L -o win11.img https://bit.ly/3i11MQ3
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip &>/dev/null &
+unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
+read -p ">>bismillah>>: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3388 &>/dev/null &
+./ngrok tcp 3388 &>/dev/null &
+apt-get install qemu > /dev/null 2>&1
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Username: hiro"
+echo "Password: telegram ke hiro"
+qemu-system-x86_64 -hda win11.img -m 12G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic  > /dev/null 2>&1
+sleep 4320000
