a7469746a1797021946d217922831d8dbe3bc3d8
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-14 00:44:03 +0700

    Update win11.sh

--- win11.sh
+++ win11.sh
@@ -1,7 +1,6 @@
 echo "####################################❤❖﷽❖❤#####################################"
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
-curl -L -o win11.zip https://public.ch.files.1drv.com/y4mZOhs57US01gbY4reSxpkOitywJLK52VTzTzzEuEPP3UcEsfkVHFEOQQZo2UItYnx02G77nZJItRMZ3TL8wIvLhLx4OV8o0BX6GwS2KCWxLQVpqtOfRGHgGqZKsRwfmkZ7IjrgVXH9H3_3ny5rah2XJNk87beX_6zjlFoWHAekhyQ2Z1Fmc8-KDzTxMIH0MbdkX5J8jn6s5BxfLU5p1GHeZNU2UtA0oSOurf-3iyFJ40
-unzip win11.zip
+curl -L -o win11.img https://bit.ly/3t7CPIV
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip &>/dev/null &
 unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
@@ -13,5 +12,5 @@ apt-get install qemu > /dev/null 2>&1
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Username: hiro"
 echo "Password: telegram ke hiro"
-qemu-system-x86_64 win11.img -m 8G > /dev/null 2>&1
+qemu-system-x86_64 win11.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 sleep 4320000
