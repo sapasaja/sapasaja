8d2e832ce1e7208494de3f2255338cbe760ac127
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-14 19:26:46 +0700

    Update win.sh

--- win.sh
+++ win.sh
@@ -1,3 +1,5 @@
+echo "####################################❤❖﷽❖❤#####################################"
+echo "********************************(khasbunalloh wanikmal wakil)*******************************"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip &>/dev/null &
 unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
