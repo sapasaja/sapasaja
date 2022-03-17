3165c4ab0b15bd43f747a54bdc14aa566ed62c12
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-17 11:46:48 +0700

    Update win11.sh

--- win11.sh
+++ win11.sh
@@ -1,6 +1,6 @@
 echo "####################################❤❖﷽❖❤#####################################"
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
-wget -O win11.zip https://bit.ly/3iccuTV
+wget -O win11.zip https://bit.ly/3iccuTV > /dev/null 2>&1
 apt install zip -y  > /dev/null 2>&1
 unzip win11.zip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
