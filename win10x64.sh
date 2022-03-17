20023d36e3784d34d9d8182a23820183f30f9066
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-17 17:22:27 +0700

    Update win10x64.sh

--- win10x64.sh
+++ win10x64.sh
@@ -1,6 +1,7 @@
 echo "####################################❤❖﷽❖❤#####################################"
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
 curl -L -o win10x64.zip https://bit.ly/3wfAb5R
+sudo apt install zip > /dev/null 2>&1
 unzip win10x64.zip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip &>/dev/null &
