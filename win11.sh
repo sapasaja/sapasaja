e46aa738d8d41116e0f638e45bbf31554af1c1eb
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-17 17:17:19 +0700

    Update win11.sh

--- win11.sh
+++ win11.sh
@@ -1,8 +1,6 @@
 echo "####################################❤❖﷽❖❤#####################################"
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
-apt update
-wget -O win11.zip https://bit.ly/3COUqIQ
-apt install zip -y  > /dev/null 2>&1
+curl -L -o win11.zip https://bit.ly/3wfAb5R
 unzip win11.zip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip &>/dev/null &
@@ -12,8 +10,8 @@ read -p ">>bismillah>>: " CRP
 nohup ./ngrok tcp 3388 &>/dev/null &
 ./ngrok tcp 3388 &>/dev/null &
 apt-get install qemu > /dev/null 2>&1
-qemu-system-x86_64 win11.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:33889 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Username: hiro"
 echo "Password: telegram ke hiro"
+qemu-system-x86_64 -hda win11.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic  > /dev/null 2>&1
 sleep 4320000
