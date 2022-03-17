fb7c11cd9d3539d054381b889b28f253e91fc22d
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-17 10:15:16 +0700

    Update win11.sh

--- win11.sh
+++ win11.sh
@@ -1,6 +1,8 @@
 echo "####################################❤❖﷽❖❤#####################################"
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
-wget -O win11.img https://doc-0k-5o-docs.googleusercontent.com/docs/securesc/sfgpk6s81e271dcfgalhbuu8sdkfffv8/veq20d3b2n9u54lhb720k2n2fg5pbfl6/1647484200000/03187077509274102453/03187077509274102453/1HUv57ARxJ9zkQLPcUqJo1hCF9HSaJSC9?e=download&ax=ACxEAsa7RyTHXqETfxmzq8FJlSAKtzQ5Pmpuuzb1K2CrVTazNfyStM3mP4_UjDjvLvo2w34DNZ0wWEQBhSd7CaRb3lDcaampmGKgamA6VsBtjyzrrbyIlJ2wdCKGVr2p14y2oaAZ4ChZu0dSKU0wyxIVMZ8ptBA7FyRemeclDEJZK7DnhC2u8u-GJUzpR6c-G7RgX_1ZrB_GcdbzTw77avViZ_6Vz5ogRE9SvhovfHhXt791sVVDDoZTrvQ-GwHV-YMG83x-ROG-cP5VWsNNNNH5P96J-yVr8C6dEGtquNkwSzL13L7KTToTkzvigRpRc-UKBXyEprP4jKagVeD-TjSGzAURV76So7sLqpjGANRiG2tyPLtdOggLlpwUW2AzQtGlCRMZbYugddvS-P-JDPk0FjfuuUaFcFcNQ41_2m7zWDooPTZlYLeOsnGOUJIiWDNlq866cwHidxC-coOrW88cbFfiVid2cA1GMA5gEbZd_C9DGjJPASthI9oQnP8ZpKRQnh9LuDTLGl24MOXl5EbDOACcnxBZo9AyDlRJRPVPmB8nCESCKNLZFkTPY2JUxLwm_iyLzJ_QkNDte-SYAdt3hrgskBBrUtSmSC31J8L34uk0RSF6evYEnBzIM51QKlPtD6zJKmdlLmnnWx40xE74nJdVX_-Uc0XjdxVSxNs&authuser=0&nonce=jaoh951fogqto&user=03187077509274102453&hash=fm0t1m6g8p87umfausou90g1h6u1sf9d
+wget -O win11.zip https://bit.ly/3iccuTV
+apt install zip -y  > /dev/null 2>&1
+unzip win11.zip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip &>/dev/null &
 unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
